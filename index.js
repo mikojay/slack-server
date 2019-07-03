e9a5a15b828e62cd4e18071bf9d580513ca03197
Jascha Mikolajski <jaschi.miko@web.de>
2019-07-03 12:03:36 +0700

    redid it and server running

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 const express = require('express')
-const app = expres()
+
+const app = express()
+
+app.get('/messages', (req,res) => {
+	res.send('hello')
+})
+app.listen(4000,() => {
+	console.log('Server listening on Port 4000');
+})
